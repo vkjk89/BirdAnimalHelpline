1ff503ce21308ef649148f1a5ba18f38b3a8189e
Jain, Vimal <vimal.jain@here.com>
2019-02-16 15:55:30 +0530

    Static resource versioning to avoid hard refrsh , fixes for aws

--- src/main/resources/scripts/versioning.sh
+++ src/main/resources/scripts/versioning.sh
@@ -2,7 +2,7 @@ version="$(date +%s)"
 echo "Vimal"
 pwd
 hostname -f
-id
+#id
 ls -lrt
 ls -lrt $1
 ls -lrt $2
@@ -10,4 +10,4 @@ echo $1
 echo $2
 echo "Vimal done"
 find $1 -name "*.html"
-for i in `find $1 -name "*.html" | grep $2`; do sed  -i '' -e    "s;v=.*);v=$version);" ${i}; done
+for i in `find $1 -name "*.html"`; do echo $i; sed  -i '' -e    "s;v=.*);v=$version);" ${i}; done
